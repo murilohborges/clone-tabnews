0919bc44d9a99b3186cd84d18edaa7d3ddef3f45
Murilo Borges <129700245+murilohborges@users.noreply.github.com>
2024-10-23 13:15:37 +0000

    correcting text from title

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>If the world was ending, I'd wanna be next to you</h1>
+    return <h1>If the world was ending, I'd wanna be next to you...</h1>
 }
 
 export default Home;
