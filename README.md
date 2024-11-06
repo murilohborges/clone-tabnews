c2728e5bbb4f49afb46d31f91a7908005b41fd56
Murilo Borges <129700245+murilohborges@users.noreply.github.com>
2024-11-06 14:00:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Treinamento pela implementação do https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Em construção...</h1>
+function Home() {
+  return <h1>Em construção...</h1>;
+}
+
+function Homesad() {
+  return <h1>Em construção...</h1>;
 }
 
 export default Home;
